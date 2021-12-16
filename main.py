1ce09537d2b08b7d986a66f2dfc995f9fdb36938
UncleVanechka <ikorkonishko@gmail.com>
2021-12-16 18:22:32 +0300

    added another

--- main.py
+++ main.py
@@ -316,7 +316,7 @@ def adding_country():
                     logger.info(response.text)
                     return redirect("/admin/adding/country")
                 else:
-                    return redirect('/admin/sign-in')
+                    return redirect('/admin/menu')
 
             else:
                 return redirect("/admin/adding/country")
@@ -346,17 +346,17 @@ def adding_airport():
         if request.method == "GET":
             return render_template('adding_airport.html')
         else:
-            body['airportName'] = request.form.get("country_id")
-            body['airportType'] = request.form.get("name")
-            body['airportCode'] = request.form.get("continent")
-            body['airportCountryId'] = request.form.get("country_wiki")
-            body['airportIsoRegion'] = request.form.get("country_id")
-            body['airportMunicipality'] = request.form.get("name")
-            body['airportHomeLink'] = request.form.get("continent")
-            body['airportVisa'] = request.form.get("country_wiki")
-            body['airportQuarantine'] = request.form.get("country_id")
-            body['airportCovidTest'] = request.form.get("name")
-            body['airportLockDown'] = request.form.get("continent")
+            body['airportName'] = request.form.get("airportName")
+            body['airportType'] = request.form.get("airportType")
+            body['airportCode'] = request.form.get("airportCode")
+            body['airportCountryId'] = int(request.form.get("airportCountryId"))
+            body['airportIsoRegion'] = request.form.get("airportIsoRegion")
+            body['airportMunicipality'] = request.form.get("airportMunicipality")
+            body['airportHomeLink'] = request.form.get("airportHomeLink")
+            body['airportVisa'] = request.form.get("airportVisa")
+            body['airportQuarantine'] = request.form.get("airportQuarantine")
+            body['airportCovidTest'] = request.form.get("airportCovidTest")
+            body['airportLockDown'] = request.form.get("airportLockDown")
 
             if body['airportCode'] is not None and body['airportCode'] != "":
 
@@ -371,7 +371,7 @@ def adding_airport():
                     logger.info(response.text)
                     return redirect("/admin/adding/airport")
                 else:
-                    return redirect('/admin/sign-in')
+                    return redirect('/admin/menu')
 
             else:
                 return redirect("/admin/adding/airport")
@@ -383,25 +383,56 @@ def adding_airport():
 @app.route('/admin/adding/plane', methods=('GET', 'POST'))
 def adding_plane():
     global access_token_admin
+
+    body = {
+    "aircraftIata": "",
+    "aircraftName" : "",
+    "aircraftManufacturer": "",
+    "aircraftType":"",
+    "aircraftIcaic": "",
+    "aircraftWingType" : "",
+    "economyClass" : "",
+    "prEconomyClass" : "",
+    "businessClass": "",
+    "firstClass":""
+}
+
+
     if (request.method == "GET" or request.method == "POST") and access_token_admin is not None:
 
         if request.method == "GET":
             return render_template('adding_plane.html')
         else:
-            aircraftIata = request.form.get("aircraftIata")
-            aircraftName = request.form.get("aircraftName")
-            aircraftManufacturer = request.form.get("aircraftManufacturer")
-            aircraftType = request.form.get("aircraftType")
+            body['aircraftIata'] = request.form.get("aircraftIata")
+            body['aircraftName'] = request.form.get("aircraftName")
+            body['aircraftManufacturer'] = request.form.get("aircraftManufacturer")
+            body['aircraftType'] = request.form.get("aircraftType")
 
-            aircraftIcaic = request.form.get("aircraftIcaic")
-            aircraftWingType = request.form.get("aircraftWingType")
-            economyClass = request.form.get("economyClass")
-            prEconomyClass = request.form.get("prEconomyClass")
+            body['aircraftIcaic'] = request.form.get("aircraftIcaic")
+            body['aircraftWingType'] = request.form.get("aircraftWingType")
+            body['economyClass'] = request.form.get("economyClass")
+            body['prEconomyClass'] = request.form.get("prEconomyClass")
 
-            businessClass = request.form.get("businessClass")
-            firstClass = request.form.get("firstClass")
+            body['businessClass'] = request.form.get("businessClass")
+            body['firstClass'] = request.form.get("firstClass")
 
-            return redirect("/admin/adding/plane")
+            if body['aircraftIata'] is not None and body['aircraftIata'] != "":
+
+                headers = {
+                    'Authorization': 'Bearer ' + access_token_admin
+                }
+                logger.info("Trying to send")
+                logger.info(headers)
+                response = requests.request("POST", 'http://gvapi:8000/v1/aircrafts', headers=headers, json=body)
+
+                if response.ok:
+                    logger.info(response.text)
+                    return redirect("/admin/adding/plane")
+                else:
+                    return redirect('/admin/menu')
+
+            else:
+                return redirect("/admin/adding/plane")
     else:
         return redirect('/admin/sign-in')
 
@@ -409,40 +440,82 @@ def adding_plane():
 @app.route('/admin/adding/flight', methods=('GET', 'POST'))
 def adding_flight():
     global access_token_admin
+    body = {
+    "flightName": "",
+    "airlineId" :  None,
+    "ticketNumEconomy": None,
+    "ticketNumPrEconomy": None,
+    "ticketNumBusiness": None,
+    "ticketNumFirstClass" : None,
+    "costRubEconomy" : None,
+    "costRubPrEconomy" : None,
+    "costRubBusiness": None,
+    "costRubFirstClass":None,
+    "aircraftId": None,
+    "airportDepId":None,
+    "airportLandId": None,
+    "departureTime" : "",
+    "landingTime" : "",
+    "maxLuggageWeightKg" : None,
+    "costLuggageWeightRub": None,
+    "maxHandLuggageWeightKg":None,
+    "costHandLuggageWeightRub":None,
+    "wifiFlg": "",
+    "foodFlg" : "",
+    "usbFlg" : ""
+}
+
+
     if (request.method == "GET" or request.method == "POST") and access_token_admin is not None:
 
         if request.method == "GET":
             return render_template('adding_flight.html')
         else:
-            flightName = request.form.get("flightName")
-            airlineId = request.form.get("airlineId")
-            ticketNumEconomy = request.form.get("ticketNumEconomy")
-            ticketNumPrEconomy = request.form.get("ticketNumPrEconomy")
+            body['flightName'] = request.form.get("flightName")
+            body['airlineId'] = int(request.form.get("airlineId"))
+            body['ticketNumEconomy'] = int(request.form.get("ticketNumEconomy"))
+            body['ticketNumPrEconomy'] = int(request.form.get("ticketNumPrEconomy"))
 
-            ticketNumBusiness = request.form.get("ticketNumBusiness")
-            ticketNumFirstClass = request.form.get("ticketNumFirstClass")
-            costRubEconomy = request.form.get("costRubEconomy")
-            costRubPrEconomy = request.form.get("costRubPrEconomy")
+            body['ticketNumBusiness'] = int(request.form.get("ticketNumBusiness"))
+            body['ticketNumFirstClass'] = int(request.form.get("ticketNumFirstClass"))
+            body['costRubEconomy'] = int(request.form.get("costRubEconomy"))
+            body['costRubPrEconomy'] = int(request.form.get("costRubPrEconomy"))
 
-            costRubBusiness = request.form.get("costRubBusiness")
-            costRubFirstClass = request.form.get("costRubFirstClass")
+            body['costRubBusiness'] = int(request.form.get("costRubBusiness"))
+            body['costRubFirstClass'] = int(request.form.get("costRubFirstClass"))
 
-            aircraftId = request.form.get("aircraftId")
-            airportDepId = request.form.get("airportDepId")
-            airportLandId = request.form.get("airportLandId")
-            departureTime = request.form.get("departureTime")
+            body['aircraftId'] = int(request.form.get("aircraftId"))
+            body['airportDepId'] = int(request.form.get("airportDepId"))
+            body['airportLandId'] = int(request.form.get("airportLandId"))
+            body['departureTime'] = request.form.get("departureTime")
 
-            landingTime = request.form.get("landingTime")
-            maxLuggageWeightKg = request.form.get("maxLuggageWeightKg")
-            costLuggageWeightRub = request.form.get("costLuggageWeightRub")
-            maxHandLuggageWeightKg = request.form.get("maxHandLuggageWeightKg")
+            body['landingTime'] = request.form.get("landingTime")
+            body['maxLuggageWeightKg'] = int(request.form.get("maxLuggageWeightKg"))
+            body['costLuggageWeightRub'] = int(request.form.get("costLuggageWeightRub"))
+            body['maxHandLuggageWeightKg'] = int(request.form.get("maxHandLuggageWeightKg"))
 
-            costHandLuggageWeightRub = request.form.get("costHandLuggageWeightRub")
-            wifiFlg = request.form.get("wifiFlg")
-            foodFlg = request.form.get("foodFlg")
-            usbFlg = request.form.get("usbFlg")
+            body['costHandLuggageWeightRub'] = int(request.form.get("costHandLuggageWeightRub"))
+            body['wifiFlg'] = request.form.get("wifiFlg")
+            body['foodFlg'] = request.form.get("foodFlg")
+            body['usbFlg'] = request.form.get("usbFlg")
 
-            return redirect("/admin/adding/flight")
+            if body['flightName'] is not None and body['flightName'] != "":
+
+                headers = {
+                    'Authorization': 'Bearer ' + access_token_admin
+                }
+                logger.info("Trying to send")
+                logger.info(headers)
+                response = requests.request("POST", 'http://gvapi:8000/v1/flights', headers=headers, json=body)
+
+                if response.ok:
+                    logger.info(response.text)
+                    return redirect("/admin/adding/flight")
+                else:
+                    return redirect('/admin/menu')
+
+            else:
+                return redirect("/admin/adding/flight")
     else:
         return redirect('/admin/sign-in')
 
@@ -450,17 +523,42 @@ def adding_flight():
 @app.route('/admin/adding/aviacompany', methods=('GET', 'POST'))
 def adding_aviacompany():
     global access_token_admin
+
+    body = {
+    "airlineCountryId": None,
+    "airlineIata" : "",
+    "airlineIcao": "",
+    "airlineActive":""
+    }
+
     if (request.method == "GET" or request.method == "POST") and access_token_admin is not None:
 
         if request.method == "GET":
             return render_template('adding_aviacompany.html')
         else:
-            aviacompany_id = request.form.get("aviacompany_id")
-            airlineIata = request.form.get("airlineIata")
-            airlineIcao = request.form.get("airlineIcao")
-            aviacompany_status = request.form.get("aviacompany_status")
 
-            return redirect("/admin/adding/aviacompany")
+            body['airlineCountryId'] = int(request.form.get("aviacompany_id"))
+            body['airlineIata'] = request.form.get("airlineIata")
+            body['airlineIcao'] = request.form.get("airlineIcao")
+            body['airlineActive'] = request.form.get("aviacompany_status")
+
+            if body['airlineCountryId'] is not None and body['airlineCountryId'] != "":
+
+                headers = {
+                    'Authorization': 'Bearer ' + access_token_admin
+                }
+                logger.info("Trying to send")
+                logger.info(headers)
+                response = requests.request("POST", 'http://gvapi:8000/v1/airlines', headers=headers, json=body)
+
+                if response.ok:
+                    logger.info(response.text)
+                    return redirect("/admin/adding/aviacompany")
+                else:
+                    return redirect('/admin/menu')
+
+            else:
+                return redirect("/admin/adding/aviacompany")
     else:
         return redirect('/admin/sign-in')
 
